c2be4d3ecc295a1e5e8f1c684d2e3f4d92f29ec7
Jhonatan <jhonatanrotter@gmail.com>
2020-08-09 20:27:31 -0500

    segundo commit

--- app/src/main/java/com/jhonkkman/app2/MainActivity.java
+++ app/src/main/java/com/jhonkkman/app2/MainActivity.java
@@ -2,13 +2,27 @@ package com.jhonkkman.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_chatear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_chatear = (Button) findViewById(R.id.btn_chatear);
+        btn_chatear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,ChatActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
